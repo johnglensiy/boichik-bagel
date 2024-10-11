5feaeb492978f1a166ca742f0be681f8efe10dc0
John Glen Siy <johnglen_siy@berkeley.edu>
2024-10-10 20:06:52 -0700

    reconfigure

---
+++ src/components/hero.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import styled from "styled-components"
+
+const StyledSection = styled.section`
+  .title {
+    margin-bottom: 0;
+  }
+  .subtitle {
+    margin-top: 0;
+  }
+  .highlighted {
+    box-shadow: inset 0 -2.5rem 0 #cdf3e1;
+  }
+`
+
+const Hero = () => {
+  return (
+    <StyledSection>Bp</StyledSection>
+  )
+}
+
+export default Hero
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+import Layout from "../components/layout.tsx"
+import Hero from "../components/layout.tsx"
+
+export default function Home() {
+  return (
+    <Layout>
+        <Hero>
+          hi
+        </Hero>
+        <h1>Hi! Boop</h1>
+    </Layout>
+  )
+}
