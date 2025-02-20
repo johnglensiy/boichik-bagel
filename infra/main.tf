418df207b686b9790c8848f4107e541f64eae48e
John Glen Siy <johnglen_siy@berkeley.edu>
2025-02-20 13:15:21 -0700

    added database to config

--- infra/main.tf
+++ infra/main.tf
@@ -30,4 +30,15 @@ resource "aws_route53_zone" "terraform_hosted_zone" {
   name = "johnglendsiy.me"
 }
 
+resource "aws_dynamodb_table" "terraform_visitor_count_db" {
+  name           = "visitor-count"
+  hash_key       = "id"  # Change this to your table's hash key
+  billing_mode   = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "id"
+    type = "N"  # Change to your key type (e.g., "S" for string, "N" for number)
+  }
+}
+
 
