121b0057ee4ae2712f9c6b7587f1d790da1669f4
John Glen Siy <johnglen_siy@berkeley.edu>
2025-11-06 16:47:56 -0800

    feat: add intro to aboutme section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,51 +13,24 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <h2>Hi! I'm John</h2>
+            <p>
+                Hi! I'm John, an undergraduate Electrical Engineering and Computer Science
+                student at <span className="berkeley-cursor">UC Berkeley</span>. I'm a full-stack engineer
+                interested in software development and building impactful projects.
+                <br/>
+                <br/>
+                In spring semester of my sophomore year, I joined a small little CS club on campus called Codeology. 
+                There I experienced my first exposure to industry level projects, and since then, I've been
+                actively contributing as a software developer working for major clients like IBM and Samsung.
+                If you're looking for large-scale software contract roles or business partnerships, 
+                I highly recommend you check them out  <a href={"https://codeology.studentorg.berkeley.edu/"} target="_blank" rel="noopener noreferrer">here</a>!
+                <br/>
+                <br/>
+                In my free time, I love bouldering (indoor v6, outdoor v4), running, and leveling up my
+                cooking game. I'm also a huge sports fan and
+                have been cheering for the Warriors since 2013 - go Dubs! <br/>
+            </p>
         </div>
       </main>
     </div>
