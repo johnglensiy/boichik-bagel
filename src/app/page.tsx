beda52382f7364c9d1dfea9befdcc18ed8a364cb
John Glen Siy <johnglen_siy@berkeley.edu>
2025-11-19 12:23:44 -0800

    feat(breaking): add dropdown projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import matter from 'gray-matter';
 
 import ViewCount from "@/components/ViewCount";
 import ProjectCard from "@/components/ProjectCard";
+import DropdownProjects from '@/components/DropdownProjects';
 
 export interface ProjectData {
   id: string,
@@ -77,11 +78,12 @@ export default function Home() {
         <div id="projects-section" className="flex flex-col border">
           <h2 className="before:content-[''] before:inline-block before:h-3 before:w-3 before:bg-[#ee5b36] before:mr-2 text-2xl font-bold my-6">Software Engineering</h2>
           <div className="flex flex-row flex-wrap">
-            {projects.filter((project) => project.category === 'software')
+            {projects.filter((project) => project.category === 'software').slice(0,3)
               .map((project) => (
               <ProjectCard key={project.id} {...project}/>
             ))}
           </div>
+
           <h2 className="before:content-[''] before:inline-block before:h-3 before:w-3 before:bg-[#ee5b36] before:mr-2 text-2xl font-bold my-6">Data Science/NLP</h2>
           <div className="flex flex-row flex-wrap">
             {projects.filter((project) => project.category === 'data')
@@ -89,6 +91,7 @@ export default function Home() {
               <ProjectCard key={project.id} {...project}/>
             ))}
           </div>
+
           <h2 className="before:content-[''] before:inline-block before:h-3 before:w-3 before:bg-[#ee5b36] before:mr-2 text-2xl font-bold my-6">Hardware/Electrical Engineering</h2>
           <div className="flex flex-row flex-wrap">
             {projects.filter((project) => project.category === 'hardware')
