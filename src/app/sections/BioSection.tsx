6e38d22945c554a2c9037235ed70d8c2471d4a10
John Glen Siy <johnglen_siy@berkeley.edu>
2025-11-23 22:28:38 -0800

    refactor: create separate files for sections

---
+++ src/app/sections/BioSection.tsx
@@ -0,0 +1,24 @@
+export default function BioSection() {
+  return (
+    <div id="about" className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+      <h2 className="text-2xl font-bold">Hi! I'm John</h2>
+      <p>
+          Hi! I'm John, an undergraduate Electrical Engineering and Computer Science
+          student at <span className="berkeley-cursor">UC Berkeley</span>. I'm a full-stack engineer
+          interested in software development and building impactful projects.
+          <br/>
+          <br/>
+          In spring semester of my sophomore year, I joined a small little CS club on campus called Codeology. 
+          There I experienced my first exposure to industry level projects, and since then, I've been
+          actively contributing as a software developer working for major clients like IBM and Samsung.
+          If you're looking for large-scale software contract roles or business partnerships, 
+          I highly recommend you check them out  <a href={"https://codeology.studentorg.berkeley.edu/"} target="_blank" rel="noopener noreferrer">here</a>!
+          <br/>
+          <br/>
+          In my free time, I love bouldering (indoor v6, outdoor v4), running, and leveling up my
+          cooking game. I'm also a huge sports fan and
+          have been cheering for the Warriors since 2013 - go Dubs! <br/>
+      </p>
+    </div>
+  )
+}
---
+++ src/app/sections/CourseworkSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CourseworkSection() {
+  return (
+    <p>these are courses</p>
+  )
+}
