5c9b35d724651c8c4e47a04690c614532de6e532
John Glen Siy <johnglen_siy@berkeley.edu>
2025-12-27 15:04:32 -0800

    style: shorten hero section

--- src/app/sections/BioSection.tsx
+++ src/app/sections/BioSection.tsx
@@ -1,21 +1,16 @@
 export default function BioSection() {
   return (
-    <div id="about" className="flex flex-col items-center my-15 gap-6 text-center sm:items-start sm:text-left">
-      <h2 className="text-2xl font-bold">Hi! I'm John</h2>
+    <div id="about" className="flex flex-col items-center my-15 gap-6 max-w-200 text-center sm:items-start sm:text-left">
+      <h2 className="text-5xl font-bold">Hi! I'm John</h2>
       <p>
-          Hi! I'm a full-stack engineer interested in software development and building impactful projects.
+          I'm a full-stack engineer interested in software development and building impactful projects.
           <br/>
           <br/>
           In spring semester of my sophomore year, I joined a small little CS club on campus called Codeology. 
-          There I experienced my first exposure to industry level projects, and since then, I've been
-          actively contributing as a software developer working for major clients like IBM and Samsung.
           If you're looking for large-scale software contract roles or business partnerships, 
-          I highly recommend you check them out  <a href={"https://codeology.studentorg.berkeley.edu/"} target="_blank" rel="noopener noreferrer">here</a>!
+          I highly recommend you check them out <u> <a href={"https://codeology.studentorg.berkeley.edu/"} target="_blank" rel="noopener noreferrer">here</a></u>!
           <br/>
-          <br/>
-          In my free time, I love bouldering (indoor v6, outdoor v4), running, and leveling up my
-          cooking game. I'm also a huge sports fan and
-          have been cheering for the Warriors since 2013 - go Dubs! <br/>
+    
       </p>
     </div>
   )
