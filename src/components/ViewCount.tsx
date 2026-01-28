fbcb870b0ca87758beceedf3e51b156329f46247
John Glen Siy <johnglen_siy@berkeley.edu>
2026-01-28 14:22:55 -0800

    fix: correct view count for unique visitors

--- src/components/ViewCount.tsx
+++ src/components/ViewCount.tsx
@@ -1,19 +1,35 @@
 'use client'
  
 import incrementViews from '@/actions/incrementViews'
+import getViews from '@/actions/getViews'
 import { useState, useEffect, useTransition } from 'react'
  
 export default function ViewCount({ initialViews }: { initialViews: number }) {
-  const [views, setViews] = useState(initialViews)
-  const [isPending, startTransition] = useTransition()
+  const [views, setViews] = useState<number | null>(null);
+  const [isPending, startTransition] = useTransition();
  
+  // Only trigger counter with unique visitors
   useEffect(() => {
-    startTransition(async () => {
-      const updatedViews = await incrementViews()
-      setViews(updatedViews)
-    })
+    const hasVisited = localStorage.getItem("hasVisited");
+
+    if (!hasVisited) {
+      localStorage.setItem("hasVisited", 'true');
+      startTransition(async () => {
+        const updatedViews = await incrementViews();
+        setViews(updatedViews);
+      }) 
+    } else {
+      startTransition(async () => {
+        const currViews = await getViews();
+        setViews(currViews);
+      })
+    }
   }, [])
  
   // You can use `isPending` to give users feedback
-  return <p>You are the <span className="font-bold">{views}th</span> visitor of my website!</p>
+  return (
+    <p>
+      You are the {views === null || isPending ? '...' : <span className='font-bold'>{views}th</span>} visitor of my website!
+    </p>
+  )
 }
